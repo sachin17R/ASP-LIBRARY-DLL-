163f7b986cfb3ea4365d691740d47c9406503993
Sachin R <121842168+sachin17R@users.noreply.github.com>
2023-02-08 19:37:46 +0530

    Add files via upload

---
+++ EmployeeLib/DataClasses/EmployeeDataComponent.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeLib.DataClasses
+{
+    public class EmployeeDataComponent : IDataClasses
+    {
+
+        private EmployeeDBEntities _context = new EmployeeDBEntities();
+        public void AddNewEmployee(Employee emp)
+        {
+            _context.Employees.Add(emp);
+            _context.SaveChanges();
+        }
+
+        public void DeleteEmployee(int empId)
+        {
+            var selected = _context.Employees.FirstOrDefault((e) => e.EmpId == empId);
+            if (selected != null)
+            {
+                _context.Employees.Remove(selected);
+                _context.SaveChanges();
+            }
+            else
+                throw new Exception("Employee Id Not Found To Delete");
+        }
+
+        public Employee FindEmp(int id)
+        {
+            var selected = _context.Employees.FirstOrDefault((p) => p.EmpId == id);
+            return selected;
+        }
+        public List<Dept> GetAllDept()
+        {
+            return _context.Depts.ToList();
+        }
+
+        public List<Employee> GetAllEmployee()
+        {
+            return _context.Employees.ToList();
+        }
+
+        public void UpdateEmployee(Employee emp)
+        {
+            var selected = _context.Employees.FirstOrDefault((e) => e.EmpId == emp.EmpId);
+            if (selected != null)
+            {
+                selected.EmpName = emp.EmpName;
+                selected.Address = emp.Address;
+                selected.Salary = emp.Salary;
+                _context.SaveChanges();
+            }
+            else
+            {
+                throw new Exception("Employee Id Not Found to Update");
+            }
+        }
+    }
+}
---
+++ EmployeeLib/DataClasses/IDataClasses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeLib.DataClasses
+{
+    interface IDataClasses
+    {
+        void AddNewEmployee(Employee emp);
+        void UpdateEmployee(Employee emp);
+        void DeleteEmployee(int empId);
+        List<Employee> GetAllEmployee();
+        List<Dept> GetAllDept();
+        Employee FindEmp(int id);
+    }
+}
