3b12ca2031cb45fd8c9ee0e5327df4bc2c1b25a6
Sachin R <121842168+sachin17R@users.noreply.github.com>
2023-02-08 19:31:45 +0530

    Add files via upload

---
+++ ProductLib/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductLib
+{
+    public interface IDbcomponents
+    {
+        void AddProduct(Product pro);
+        void UpdateProduct(Product pro);
+        void removeProduct(int id);
+        List<Product> GetAllProduct();
+    }
+
+    public static class ProductFactory
+    {
+      public static IDbcomponents GetComponents() => new DbProduct();
+    }
+      class DbProduct : IDbcomponents
+    {
+        static Entities context = new Entities();
+        public void AddProduct(Product pro)
+        {
+            context.Products.Add(pro);
+            context.SaveChanges();
+        }
+
+        public List<Product> GetAllProduct()
+        {
+           return context.Products.ToList();
+        }
+
+        public void removeProduct(int id)
+        {
+            var found = context.Products.FirstOrDefault((p) => p.ProductId == id);
+            context.Products.Remove(found);
+            context.SaveChanges();
+        }
+
+        public void UpdateProduct(Product pro)
+        {
+            var found = context.Products.First((p) => p.ProductId == pro.ProductId);
+            found.ProductImage = pro.ProductImage;
+            found.ProductName = pro.ProductName;
+            found.Price = pro.Price;
+            found.Quantity = pro.Quantity;
+            context.SaveChanges();
+        }
+    }
+    
+}
